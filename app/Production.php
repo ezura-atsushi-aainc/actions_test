611d73cf7781424222040859980fb5f1629fb1d2
EZURA, Atsushi <ezura.atsushi@aainc.co.jp>
2025-05-30 18:35:16 +0900

    add

--- app/Production.php
+++ app/Production.php
@@ -34,8 +34,9 @@ class Production
         return (string) $this->y;
     }
 
-    public function getY(): int
-    {
-        return $this->z;
+    public function
+      getY(): int
+        {
+                      return $this->z;
     }
 }
