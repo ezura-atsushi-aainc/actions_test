2e72fc561d6b480c2e8d27f3cccfce26ec076cda
ezura-atsushi-aainc <85330749+ezura-atsushi-aainc@users.noreply.github.com>
2025-05-30 08:14:43 +0000

    ci: apply php-cs-fixer auto fixes

--- app/Production.php
+++ app/Production.php
@@ -1,42 +1,41 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Production;
 
 class Production
 {
-   /** @var int */
-               private
-    
-    
-    $x;
+    /** @var int */
+    private $x;
 
-       /** @var int */
-    private
-      $y;
+    /** @var int */
+    private $y;
 
-  /** @var int */
-      private $z;
+    /** @var int */
+    private $z;
 
-      public function __construct()
+    public function __construct()
     {
-                 $this->x = 1;
+        $this->x = 1;
         $this->y = 2;
 
-            $this->z
-                    = 3;
+        $this->z
+                = 3;
     }
 
-  public function getX(): int {
-          return $this->x; }
-
-        public function getY(): int
-  {
-        return (string)$this->y;
-        }
+    public function getX(): int
+    {
+        return $this->x;
+    }
 
     public function getY(): int
     {
-    return
-            $this->z;
+        return (string) $this->y;
+    }
+
+    public function getY(): int
+    {
+        return $this->z;
     }
 }
