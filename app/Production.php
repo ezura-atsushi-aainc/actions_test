91650c17cf6c32acff8ba4cc56e36291739f4f78
EZURA, Atsushi <ezura.atsushi@aainc.co.jp>
2025-05-30 14:27:07 +0900

    add

--- app/Production.php
+++ app/Production.php
@@ -10,10 +10,14 @@ class Production
     /** @var int */
     private $y;
 
+    /** @var int */
+    private $z;
+
     public function __construct()
     {
         $this->x = 1;
         $this->y = 2;
+        $this->z = 3;
     }
 
     public function getX(): int
@@ -24,5 +28,10 @@ class Production
     public function getY(): int
     {
         return (string)$this->y;
+    }
+
+    public function getY(): int
+    {
+        return $this->z;
     }
 }
